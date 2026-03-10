c27f962df69f318f5e085334ba5500d09562462a
madara88645 <mehmet.ozel2701@gmail.com>
2026-03-10 11:08:20 +0000

    test: fix test_offline_new_heuristics legitimate failure caused by new deduplicator

--- tests/test_offline_new_heuristics.py
+++ tests/test_offline_new_heuristics.py
@@ -2,7 +2,9 @@ from app.compiler import compile_text_v2
 
 
 def test_new_heuristics_integration():
-    ir2 = compile_text_v2("Extract a list of items to JSON, make it short but very detailed.")
+    ir2 = compile_text_v2(
+        "Extract a list of items to a table, make it short but very detailed.", offline_only=True
+    )
     constraints_v2_text = " ".join([c.text for c in ir2.constraints])
 
     assert "No conversational filler" in constraints_v2_text
