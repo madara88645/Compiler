9d747ac41f0d9fc172e9d77b152d430532ade36d
madara88645 <163588475+madara88645@users.noreply.github.com>
2026-04-10 16:53:54 +0000

    Tighten CORS allowed methods

--- api/main.py
+++ api/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=allow_origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["*"],
 )
 
